c6a873576998a4dc607f5a6dc1b8f1ba2effd0e2
graveh <graveh@pgr.reading.ac.uk>
2026-05-01 18:50:57 +0100

    Change noise generation to simulate the same Wiener process for every dt

--- simulated_data.py
+++ simulated_data.py
@@ -31,50 +31,73 @@ def outgoing_radiation_linear(T):
 
 
 @numba.njit
-def simulate(T0, dt, n_steps, save_every_steps, sigma, rho):
+def simulate(T0, dt, dt_min, n_steps, save_every_steps, sigma, rho, seed):
+    np.random.seed(seed)
+
     sqrt_1_minus_rho_sq = np.sqrt(1 - rho**2)
-    sigma_times_sqrt_dt = sigma * np.sqrt(dt)
+    sigma_times_sqrt_dt_min = sigma * np.sqrt(dt_min)
 
     n_ensemble = T0.size
     n_save = n_steps // save_every_steps + 1
+    n_fine_per_step = int(round(dt / dt_min))
 
     T = T0.copy()
-    out_T = np.empty((n_save, n_ensemble))
-    out_T[0, :] = T
-    out_incoming = np.empty((n_save, n_ensemble))
-    out_incoming[0, :] = 0
-    out_outgoing = np.empty((n_save, n_ensemble))
-    out_outgoing[0, :] = 0
-    out_noise_incoming = np.empty((n_save, n_ensemble))
-    out_noise_incoming[0, :] = 0
-    out_noise_outgoing = np.empty((n_save, n_ensemble))
-    out_noise_outgoing[0, :] = 0
 
-    sqrt_dt = np.sqrt(dt)
-    save_idx = 1
+    out_T = np.empty((n_save, n_ensemble))
+    out_incoming = np.empty((n_save, n_ensemble))
+    out_outgoing = np.empty((n_save, n_ensemble))
+    out_noise_incoming = np.empty((n_save, n_ensemble))
+    out_noise_outgoing = np.empty((n_save, n_ensemble))
+
+    out_T[0, :] = T
+    out_incoming[0, :] = 0
+    out_outgoing[0, :] = 0
+    out_noise_incoming[0, :] = 0
+    out_noise_outgoing[0, :] = 0
 
     incoming_noise_sum = np.zeros(n_ensemble)
     outgoing_noise_sum = np.zeros(n_ensemble)
     incoming_sum = np.zeros(n_ensemble)
     outgoing_sum = np.zeros(n_ensemble)
 
+    step_noise_incoming = np.zeros(n_ensemble)
+    step_noise_outgoing = np.zeros(n_ensemble)
+
+    save_idx = 1
+
     for n in range(n_steps):
+        step_noise_incoming[:] = 0
+        step_noise_outgoing[:] = 0
+
+        # Generate fine noise in pathwise-consistent order
+        for _ in range(n_fine_per_step):
+            for i in range(n_ensemble):
+                dn_in, dn_out = generate_noise_terms(
+                    sigma_times_sqrt_dt_min,
+                    rho,
+                    sqrt_1_minus_rho_sq
+                )
+                step_noise_incoming[i] += dn_in
+                step_noise_outgoing[i] += dn_out
+
+        # Update ensemble members
         for i in range(n_ensemble):
-            # Compute
-            noise_incoming, noise_outgoing = generate_noise_terms(sigma_times_sqrt_dt, rho, sqrt_1_minus_rho_sq)
             incoming = dt * albedo_simple(T[i])
             outgoing = dt * outgoing_radiation_linear(T[i])
 
-            # Accumulate
-            incoming_noise_sum += noise_incoming
-            outgoing_noise_sum += noise_outgoing
-            incoming_sum += incoming
-            outgoing_sum += outgoing
+            incoming_sum[i] += incoming
+            outgoing_sum[i] += outgoing
+            incoming_noise_sum[i] += step_noise_incoming[i]
+            outgoing_noise_sum[i] += step_noise_outgoing[i]
 
-            # Update
-            T[i] += (incoming - outgoing + noise_incoming - noise_outgoing)
+            T[i] += (
+                incoming
+                - outgoing
+                + step_noise_incoming[i]
+                - step_noise_outgoing[i]
+            )
 
-        if n % save_every_steps == 0:
+        if (n + 1) % save_every_steps == 0:
             out_T[save_idx, :] = T
             out_incoming[save_idx, :] = incoming_sum
             out_outgoing[save_idx, :] = outgoing_sum
@@ -116,22 +139,50 @@ def add_data(df, dt, timestamps, T, incoming, outgoing,
 def main():
     # Note: for arbitrary dt values we'll need to save by testing "time > n seconds"
     # and then record also the elapsed time since last save to later normalise the change and noise terms.
-    dts = [1., 1./2, 1./4, 1./8, 1./16, 1./32, 1./64, 1./128, 1./256, 1./512, 1./1024]
-    T0 = np.array([6.])  # Implicit ensemble size of 1
+    dts = [1., 1./2, 1./4, 1./8, 1./16, 1./32,
+           1./64, 1./128, 1./256, 1./512, 1./1024,
+           1./2048, 1./4096, 1./8192]
+
+    T0 = np.array([6.])
     total_time = 1000
     sigma = 1
     rho = 0.6
+    seed = 1995
+
+    dt_min = min(dts)
+
     df = None
     timestamps = np.arange(total_time + 1)
 
-    np.random.seed(1995)
     for dt in dts:
-        n_steps = int(total_time / dt)
-        save_every_steps = int(1 / dt)
-        print(f"Simulating with dt = {dt}...", end=' ')
-        T, incoming, outgoing, noise_incoming, noise_outgoing = simulate(T0, dt, n_steps, save_every_steps, sigma, rho)
+        n_steps = int(round(total_time / dt))
+        save_every_steps = int(round(1 / dt))
+
+        print(f"Simulating with dt = {dt}...", end=" ")
+
+        T, incoming, outgoing, noise_incoming, noise_outgoing = simulate(
+            T0,
+            dt,
+            dt_min,
+            n_steps,
+            save_every_steps,
+            sigma,
+            rho,
+            seed
+        )
+
         print("done.")
-        df = add_data(df, dt, timestamps, T, incoming, outgoing, noise_incoming, noise_outgoing)
+
+        df = add_data(
+            df,
+            dt,
+            timestamps,
+            T,
+            incoming,
+            outgoing,
+            noise_incoming,
+            noise_outgoing
+        )
 
     df["sigma"] = sigma
     df["rho"] = rho
